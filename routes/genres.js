a62a482d5971a2a0a5bd51b002abfeefed86c34c
Kamil Olszewski <kamil.web@gmail.com>
2020-10-25 23:49:52 +0100

    init

--- routes/genres.js
+++ routes/genres.js
@@ -21,6 +21,9 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   let genre = new Genre({ name: req.body.name });
   genre = await genre.save();
 
@@ -51,11 +54,19 @@ router.delete("/:id", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const genre = await Genre.findById(req.params.id);
-
+  console.log(genre);
   if (!genre)
     return res.status(404).send("The genre with the given ID was not found.");
 
   res.send(genre);
 });
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(genre);
+}
 
 module.exports = router;
